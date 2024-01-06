be24bd66f163037c301e614868da8fe6cc698c60
Robert Walaski <robert@walaski.cz>
2024-01-07 00:09:50 +0100

    feature: first working version

---
+++ README.md
@@ -0,0 +1,18 @@
+# git-merge.sh
+
+Merges feature branch into main branch as one commit.
+
+## How it works
+- it finds main branch, which can be `master` or `main`
+- checks if there are all changes commited in main and feature branch
+- finds last common commit between main branch and feature branch
+- copies all changes from feature branch to main branch
+- push changes in main branch to remote repository
+
+## Install
+- clone this repo to your local machine
+- make symlink to `git-merge.sh` in your `$PATH`
+
+## TODO
+- [ ] standard output redirect to log file
+- [ ] allow delete log file when no errors
---
+++ git-merge.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Check if exists branch 'master' and 'main' and store to variable
+MAIN_BRANCH="main";
+EXISTS_BRANCH_MASTER=$(git branch -a | grep -E "master");
+EXISTS_BRANCH_MAIN=$(git branch -a | grep -E "main");
+
+# if exists both branches show error
+if [[ -n $EXISTS_BRANCH_MASTER && -n $EXISTS_BRANCH_MAIN ]]; then
+   echo "Error: branches 'master' and 'main' found";
+   exit 1;
+fi
+
+# assign not empty branch to MAIN_BRANCH
+if [[ -n $EXISTS_BRANCH_MASTER ]]; then
+   MAIN_BRANCH="master";
+fi
+echo MAIN_BRANCH: $MAIN_BRANCH;
+
+# Show info
+echo -e "Merge branch feature branch to '$MAIN_BRANCH' branch\n";
+
+# Get current branch
+CURRENT_BRANCH=$(git branch --show-current);
+if( [ "$CURRENT_BRANCH" == "$MAIN_BRANCH" ] ); then
+    echo "You must be NOT on '$MAIN_BRANCH' branch";
+    exit 1;
+fi
+
+# Check if current branch is dirty
+if [[ -n $(git status -s) ]]; then
+   echo "You must first commit last changes...";
+   exit 2;
+fi
+
+# Check if current branch is behind remote
+if [[ -n $(git status -sb | grep behind) ]]; then
+   echo "You must first pull changes from remote...";
+   exit 3;
+fi
+
+# Check if current branch need to be pushed
+if [[ -n $(git status -sb | grep ahead) ]]; then
+   echo "You must first push changes to remote...";
+   exit 4;
+fi
+
+# Do merge section
+LAST_COMMON_COMMIT=$(git merge-base $MAIN_BRANCH $CURRENT_BRANCH);
+
+# Get last commit message
+LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B);
+
+# Input readline with default value
+echo -e "ENTER COMMIT MESSAGE:"
+read -e -i "$LAST_COMMIT_MESSAGE" COMMIT_MESSAGE
+
+echo -e "Merging...\n"
+
+# Do actions
+git reset $LAST_COMMON_COMMIT;
+git stash
+git checkout $MAIN_BRANCH
+git stash pop
+git add -A
+git commit -m "$COMMIT_MESSAGE"
+git push origin
+git checkout $CURRENT_BRANCH
+git pull origin
+git checkout $MAIN_BRANCH
+echo "Done."
